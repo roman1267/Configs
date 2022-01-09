1e7140d465067c0c7b5fa9d13f6d5f2920140122
roman1267 <93879348+roman1267@users.noreply.github.com>
2022-01-09 03:02:29 -0600

    Updated install.sh

--- install.sh
+++ install.sh
@@ -11,24 +11,22 @@ error() { \
 
 sudo pacman --noconfirm --needed -Syu dialog || error "Error Upgrading or Syncing pkgs and repos."
 
+mkdir yay; cd yay/
+
+git pull https://aur.archlinux.org/yay-git.git
+
 dialog --colors --title "\Z7\ZbStay near your computer!" --yes-label "Continue" --no-label "Exit" --yesno "\Z4This script is not allowed to be run as root, but you will be asked to enter your sudo password at various points during this installation. This is to give PACMAN the necessary permissions to install the software.  So stay near the computer." 8 60
 
-sudo pacman --noconfirm --needed -Syu - < neededpkglist.txt
+yay --noconfirm --needed -Syu - < neededpkglist.txt
 
-while true; do
-	read -p "Do you want the extra packages in extrapkglist.txt? [Y/n]" yn
-		case $yn in
-			"" ) ;&
-			[Yy]* ) sudo pacman --noconfirm --needed -Syu - < extrapkglist.txt; break;
-			[Nn]* ) break;;
-			* ) echo "please answer yes or no.";;
-		esac
+read -p "Do you want the extra packages in extrapkglist.txt? [Y/n]" yn
+	case $yn in
+		"" ) ; yay --noconfirm --needed -Syu < extrapkglist.txt;;
+		[Yy]* ) yay --noconfirm --needed -Syu - < extrapkglist.txt;;
+		[Nn]* ) break;;
+	esac
 done
 
 mkdir -p $HOME/.config/
 
 cp -rv $HOME/Configs/* $HOME/.config
-
-cd $HOME/.config
-
-rm extrapkglist.txt; rm neededpkglist.txt; rm README.md
