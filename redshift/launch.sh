b64ba159e36579ea1e34a3a21629cf8e36c84045
roman1267 <romanxmaciel@gmail.com>
2022-01-08 19:40:06 -0600

    Configs

---
+++ redshift/launch.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#kills redshift
+killall -q redshift
+killall -q redshift
+killall -q redshift
+killall -q redshift
+killall -q redshift
+killall -q redshift
+killall -q redshift
+killall -q redshift
+killall -q redshift
+killall -q redshift
+killall -q redshift
+killall -q redshift
+killall -q redshift
+killall -q redshift
+redshift -x
+#Start redshift
+redshift -l 30.3:95.5 -t 3600:2300 -g .85 -m randr
+
+
+
+
+
+
+
+
+
