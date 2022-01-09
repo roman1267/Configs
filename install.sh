b40dad780d5198cf603071ce46ca5b901cb8c804
roman1267 <93879348+roman1267@users.noreply.github.com>
2022-01-09 03:08:17 -0600

    Updated git clone

--- install.sh
+++ install.sh
@@ -5,15 +5,15 @@ if [ "$(id -u)" = 0 ]; then
 	exit
 fi
 
+cd $HOME
+
 error() { \
 	    clear; printf "ERROR:\\n%s\\n" "$1" >&2; exit 1;
 }
 
 sudo pacman --noconfirm --needed -Syu dialog || error "Error Upgrading or Syncing pkgs and repos."
 
-mkdir yay; cd yay/
-
-git pull https://aur.archlinux.org/yay-git.git
+git clone https://aur.archlinux.org/yay-git.git
 
 dialog --colors --title "\Z7\ZbStay near your computer!" --yes-label "Continue" --no-label "Exit" --yesno "\Z4This script is not allowed to be run as root, but you will be asked to enter your sudo password at various points during this installation. This is to give PACMAN the necessary permissions to install the software.  So stay near the computer." 8 60
 
