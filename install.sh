147f0f7d5b5e26a32454c3b1ed589cccecf107dc
roman1267 <romanxmaciel@gmail.com>
2022-01-09 14:33:57 -0600

    reworked packaging system

--- install.sh
+++ install.sh
@@ -11,20 +11,21 @@ error() { \
 	    clear; printf "ERROR:\\n%s\\n" "$1" >&2; exit 1;
 }
 
-sudo pacman --noconfirm --needed -Syu dialog || error "Error Upgrading or Syncing pkgs and repos."
+sudo pacman --noconfirm --needed -Syu dialog base-devel || error "Error Upgrading or Syncing pkgs and repos."
 
 git clone https://aur.archlinux.org/yay-git.git
 
+sudo chown -R roman:roman ./yay-git
+
+cd yay-git
+
+makepkg -si
+
 dialog --colors --title "\Z7\ZbStay near your computer!" --yes-label "Continue" --no-label "Exit" --yesno "\Z4This script is not allowed to be run as root, but you will be asked to enter your sudo password at various points during this installation. This is to give PACMAN the necessary permissions to install the software.  So stay near the computer." 8 60
 
-yay --noconfirm --needed -Syu - < neededpkglist.txt
+yay --noconfirm --needed -Syu - < yay-pkg.txt 
 
-read -p "Do you want the extra packages in extrapkglist.txt? [Y/n]" yn
-	case $yn in
-		"" ) yay --noconfirm --needed -Syu - < ~/Configs/extrapkglist.txt;;
-		[Yy]* ) yay --noconfirm --needed -Syu - < ~/Configs/extrapkglist.txt;;
-		[Nn]* ) break;;
-	esac
+sudo pacman --noconfirm --needed -Syu - < pacman-pkg.txt
 
 mkdir -p $HOME/.config/
 
