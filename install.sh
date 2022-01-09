ba0945a9f3fe19d25a59fec005b16b61fa447569
roman1267 <93879348+roman1267@users.noreply.github.com>
2022-01-09 00:53:55 -0600

    Fixed perm issue in line 12

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ sudo pacman --noconfirm --needed -Syu dialog || error "Error Upgrading or Syncin
 
 dialog --colors --title "\Z7\ZbStay near your computer!" --yes-label "Continue" --no-label "Exit" --yesno "\Z4This script is not allowed to be run as root, but you will be asked to enter your sudo password at various points during this installation. This is to give PACMAN the necessary permissions to install the software.  So stay near the computer." 8 60
 
-pacman --noconfirm --needed -Syu - < neededpkglist.txt
+sudo pacman --noconfirm --needed -Syu - < neededpkglist.txt
 
 while true; do
 	read -p "Do you want the extra packages in extrapkglist.txt? [Y/n]" yn
